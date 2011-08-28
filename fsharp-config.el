ed0fdf1da65fcdbea19b2db8f92b0799ce836f61
Bryan Hunter <bryan.hunter@fireflylogic.com>
2011-08-27 22:56:08 -0500

    Patched more bad paths in ".el" files.

--- fsharp-config.el
+++ fsharp-config.el
@@ -1,4 +1,4 @@
-(setq load-path (cons "~/emacs-config/fsharp" load-path))
+(setq load-path (cons "~/ErlangRigEmacsConfig/fsharp" load-path))
 (setq auto-mode-alist (cons '("\\.fs[iylx]?$" . fsharp-mode) auto-mode-alist))
 (autoload 'fsharp-mode "fsharp" "Major mode for editing F# code." t)
 (autoload 'run-fsharp "inf-fsharp" "Run an inferior F# process." t)
